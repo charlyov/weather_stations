7eb9f54acb943d25a8aca87fc3b9b276c83b9401
Luis Olivares <45223906+OlivaresLD@users.noreply.github.com>
2020-05-21 19:27:47 -0500

    Update funcion_install_packs.R

--- funcion_install_packs.R
+++ funcion_install_packs.R
@@ -1,6 +1,6 @@
 ##### INSTALACIÓN Y CARGA AUTOMÁTICA DE PAQUETES EN R #####
 paquetes <- function(p){
-  new <- p[!(p %in% installed.packages()[,"Package"])]
+  new <- p[!(p %in% installed.packages() )]
   if (length(new)) install.packages(new)
   
   do.call(require, as.list(p))
