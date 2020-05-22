ca474e9cd2a8cbbe8b4f4122dd019cd118460fe5
Luis Olivares <45223906+OlivaresLD@users.noreply.github.com>
2020-05-21 19:21:53 -0500

    Update funcion_install_packs.R
    less command lines and easier computing

--- funcion_install_packs.R
+++ funcion_install_packs.R
@@ -1,10 +1,8 @@
 ##### INSTALACIÓN Y CARGA AUTOMÁTICA DE PAQUETES EN R #####
 paquetes <- function(p){
-  for(i in 1:length(p)){
-    if (p[i] %in% installed.packages()) do.call(require, list(p[i]))
-    else do.call(install.packages, list(p[i])); do.call(require, list(p[i]))
+  new <- p[!(p %in% installed.packages()[,"Package"])]
+  if (length(new)) install.packages(new)
+  
+  do.call(require, as.list(p))
   }
 }
-
-jeje <- c("shiny","ggplot2","flexdashboard","leaflet","leaflet.extras","lubridate","stringr","htmltools")
-paquetes(jeje)
